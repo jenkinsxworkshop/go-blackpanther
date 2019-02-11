1b68f7ef744e522b33ff9c1a786808d2e89c4aab
Juni Mukherjee <38925583+junibutler@users.noreply.github.com>
2019-02-11 11:04:29 -0800

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example"
+	title := "Jenkins X Black Panther!"
 
 	from := ""
 	if r.URL != nil {
